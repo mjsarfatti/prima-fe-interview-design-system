c0e3e41e38b57dddae6a0970f4ee0d96a21f1b5f
Manuele Sarfatti <mjsarfatti@gmail.com>
2024-09-22 19:42:04 +0200

    fix badge color

--- src/components/badge/Badge.tsx
+++ src/components/badge/Badge.tsx
@@ -9,7 +9,8 @@ export const Badge = styled.span<IBadge>`
   display: inline-block;
   font-size: ${(props) => props.theme.font.size.small};
   font-weight: bold;
-  line-height: 1.5;
+  color: ${(props) => props.theme.color.slate["10"]};
+  line-height: ${(props) => props.theme.font.height};
   border-radius: 18px;
   padding: ${(props) =>
     props.theme.spacing["3xs"] + " " + props.theme.spacing["2xs"]};
